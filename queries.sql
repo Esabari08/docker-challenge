f476957f2be68bd7c3435ca8797cde11da0205c4
Esabari08 <rockingrockstar612@gmail.com>
2024-08-24 06:47:07 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -15,7 +15,9 @@ CREATE TABLE `employees` (
 CREATE TABLE `infromation_schema` (
 	`schema` varchar(255) NOT NULL
 );
-
+CREATE TABLE `flag` (
+	`flag` varchar(255) NOT NULL
+);
 INSERT INTO `infromation_schema` VALUES ('OTBmNjg3MDRjN2NlODkwZDI4NTFkNzU3NTQ4MTM3OGVlYzJlMmU3Mw==');
 INSERT INTO `employees` VALUES (1,'Norma R Abney','1552 Isaacs Creek Road\r\nCatlin, IL 61817',1337);
 INSERT INTO `employees` VALUES (2,'Donald K Burns','2463 Linda Street\r\nPhiladelphia, PA 19103',31337);
@@ -23,3 +25,5 @@ INSERT INTO `employees` VALUES (3,'Brandon Young','1546 Peck Street\r\nNashua, N
 INSERT INTO `employees` VALUES (4,'Javier P. Garcia','R Doutor Alfredo\r\n7940-101 CUBA',1288);
 INSERT INTO `employees` VALUES (5,'Andrea Wurfel','R Patrão Caramelho 62\r\n4485-089 FAJOZES',3317);
 INSERT INTO `employees` VALUES (6,'Melanie Bergmann','R Padre António Vieira 60\r\n8600-166 LUZ',1022);
+
+INSERT INTO `flag` VALUES ('327a6c4304ad5938eaf0efb6cc3e53dc');
